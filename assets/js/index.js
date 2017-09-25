eed7868bf921257d4be3f24a710b0c54a433dc7e
Igor <igor@Igors-MacBook-Pro.local>
2017-09-25 17:00:30 +0300

    svgo comporess and add showMore

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,21 @@
-// Application javascript ES6;
+// Import grid;
 import isotopeGrid from './isotopeGrid';
-
+// Init isotope grid; 
 $(document).ready(isotopeGrid);
+
+// Show more 
+const $items = $('.js-moreProducts');
+const $btnShowItems = $('.js-showMore');
+
+$(document).ready(() => {
+  $items.css({
+    display: 'none',
+  });
+
+  $btnShowItems.on('click', event => {
+    event.preventDefault();
+    $items.css({
+      display: 'block',
+    });
+  });
+});
